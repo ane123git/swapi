c2e5255f6f568471e33ee1b4b5e69622d62332f0
ane123git <58523914+ane123git@users.noreply.github.com>
2019-12-05 12:27:09 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,5 +11,5 @@ import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(reducers, applyMiddleware(reduxThunk));
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
